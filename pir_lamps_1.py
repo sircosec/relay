1e946ae7ff56b5d97a5e0db51360d00a4d722b7e
Matt George <25313361+sircosec@users.noreply.github.com>
2020-01-06 21:03:46 -0500

    Add files via upload

---
+++ pir_lamps_1.py
@@ -0,0 +1,141 @@
+import RPi.GPIO as GPIO
+import time
+
+# variable which controls the time interval between the lamps turning on and off
+sleeptime = .25
+# variable for tracking the condition of the lamps (True/False)
+lamps_on = False
+# variable for tracking the condition of the pir sensor (motion True/False)
+motion = False
+# variable which controls how long (in 5 second intervals) the lamps should stay on for when motion is detected.
+# default: 60 * 5 = 300 seconds (5 minutes)
+intervals = 60
+# variable to control the time interval between checks of the pir motion sensor
+# default: 5 seconds
+# note: this gets multiplied by intervals in the state_on_loop() function
+wait = 5
+# use BCM mode for addressing the pins
+GPIO.setmode(GPIO.BCM)
+# assigning the pins variable names to make coding them simpler
+lamp1 = 26
+lamp2 = 19
+lamp3 = 13
+lamp4 = 6
+lamps = [lamp1,lamp2,lamp3,lamp4]
+# assign the pir sensor input pin a variable to make coding it simpler
+pirPin = 5
+# example code for turning lamps on and off
+# lampx ON
+# GPIO.output(i, GPIO.LOW)
+# lampx OFF
+# GPIO.output(i, GPIO.HIGH)
+
+def lamp_on(lamp_number):
+    print("setting", lamp_number, "to ON")
+    GPIO.output(lamp_number, GPIO.LOW)
+    return
+
+def lamp_off(lamp_number):
+    print("setting", lamp_number, "to OFF")
+    GPIO.output(lamp_number, GPIO.HIGH)
+    return
+
+def all_lamps_on_forward():
+    print("setting all lamps ON FORWARD")
+    for lamp in lamps:
+        lamp_on(lamp)
+        time.sleep(sleeptime)
+    lamps_on = True
+    print("lamps are ON")
+    return lamps_on
+
+def all_lamps_off_reverse():
+    print("setting all lamps OFF REVERSE")
+    for lamp in lamps[::-1]:
+        lamp_off(lamp)
+        time.sleep(sleeptime)
+    lamps_on = False
+    print("lamps are OFF")
+    return lamps_on
+
+def gpio_setup():
+    # configure the GPIO pins
+    print("setting pir pin as INPUT")
+    GPIO.setup(pirPin, GPIO.IN)
+    GPIO.setwarnings(False)
+    print("setting all lamps as OUTPUT")
+    for lamp in lamps:
+        print("setting", lamp,"to OUTPUT")
+        GPIO.setup(lamp, GPIO.OUT)
+
+def pir_check_for_motion():
+    # check the pir sensor for motion
+    if GPIO.input(pirPin) == 1:
+        print("motion detected TRUE")
+        return True
+    else:
+        print("motion detected FALSE")
+        return False
+
+def check_lamps_condition():
+    if lamps_on == True:
+        return True
+    else:
+        return False
+
+def state_off_loop():
+    """ run this loop when the lamps are off, which decides 
+    whether to keep the lamps off, or turn them on"""
+    while True:
+        motion = pir_check_for_motion()
+        if motion == False:
+            # check every 1 second for motion
+            print("motion detected is FALSE and lamps are OFF, taking no action")
+            time.sleep(1)
+        else:
+            print("motion detected is TRUE and lamps are OFF, turning lamps ON")
+            all_lamps_on_forward()
+            state_on_loop()
+
+def state_on_loop():
+    """ run this loop when the lamps are on, which decides 
+    whether to keep the lamps on, or turn them off"""
+    iterations = 0
+    print("iterations is now", iterations)
+    while True:
+        motion = pir_check_for_motion()
+        if motion == True:
+            print("motion detected is TRUE and lamps are ON")
+            print("resetting iterations to 0 and sleeping", wait,"seconds")
+            iterations = 0
+            time.sleep(wait)
+        else:
+            print("motion detected is FALSE and lamps are ON")
+            if iterations == intervals:
+                print("iterations is now", intervals)
+                print("iterations has reached", intervals)
+                print("turning lamps OFF")
+                all_lamps_off_reverse()
+                state_off_loop()
+            else:
+                print("iterations has not yet reached", intervals)
+                iterations = iterations + 1
+                print("iterations is now", iterations)
+                time.sleep(wait)
+           
+def main():
+    try:
+        # initial GPIO setup
+        gpio_setup()
+        # start by turning the lamps on and off again just because
+        lamps_on = all_lamps_on_forward()
+        lamps_on = all_lamps_off_reverse()
+        state_off_loop()
+
+    except KeyboardInterrupt:
+        print("/n")
+        print("keyboard interrupt detected!")
+        GPIO.cleanup()
+
+if __name__ == "__main__":
+    main()
